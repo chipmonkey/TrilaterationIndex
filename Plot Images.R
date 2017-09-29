53a2f7eec47dfb7727711c8990e6d4ea4329804e
Chip Lynch <chip@chiplynch.com>
2017-09-29 11:15:07 -0400

    Built 2D Simple Trilateration calculations and sample data

--- Plot Images.R
+++ Plot Images.R
@@ -24,3 +24,28 @@ polygon(myTri[,2], myTri[,3], border="blue")
 draw.circle(x = 50, y = 0, radius = 1, lwd = 1, col="red")
 draw.circle(x = 25*(2+sqrt(3)), y = tan(pi/3)*((25*(2+sqrt(3)))-50), radius = 1, lwd = 1, col="red")
 draw.circle(x = -25*(sqrt(3)-2), y = tan(-pi/3)*((-25*(sqrt(3)-2))-50), radius = 1, lwd = 1, col="red")
+
+
+# 2D Examples:
+  # Distance function:
+  myDist <- function(x, y) {
+    # x and y must be 2D points -- (x,y) tuples i.e.: x <- c(1,2) y <- c(3,4)
+    
+    d <- sqrt((x[[1]]-y[[1]])^2+(x[[2]]-y[[2]])^2)
+    
+  }
+
+  # 10 Random Points
+  myPoints <- data.frame(x = runif(n = 10, min = 0, max = 100),
+                   y = runif(n = 10, min = 0, max = 100))
+  
+  myTrilat <- data.frame(d1=NULL, d2=NULL, d3=NULL)
+  for(i in 1:nrow(myPoints)) {
+    myTrilat[i, 'd1'] <- myDist(c(myPoints[i,'x'], myPoints[i, 'y']), c(myTri[1,'x'], myTri[1,'x']))
+    myTrilat[i, 'd2'] <- myDist(c(myPoints[i,'x'], myPoints[i, 'y']), c(myTri[2,'x'], myTri[2,'x']))
+    myTrilat[i, 'd3'] <- myDist(c(myPoints[i,'x'], myPoints[i, 'y']), c(myTri[3,'x'], myTri[3,'x']))
+  }
+   
+  myTrilateration <- cbind(myPoints, myTrilat)
+  myTrilateration
+  
