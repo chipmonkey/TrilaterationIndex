228f4c972d6c6343e2f87bd83f79a042280341a1
Chip Lynch <chip@chiplynch.com>
2021-05-15 14:26:10 -0700

    Add additional research paper

--- postgresql/manual_NAP_queries.sql
+++ postgresql/manual_NAP_queries.sql
@@ -129,3 +129,82 @@ select count(m.sampleid) as mcount,
 -- 93.50%
 -- Time: 25410.502 ms (00:25.411)
 
+
+
+
+localhost:5432 postgres@postgres=# select count(m.sampleid) as mcount,
+localhost postgres@postgres- #         count(t2.sampleid) as tcount
+localhost postgres@postgres- #     from sample_cat_ref_dists m
+localhost postgres@postgres- #     left join lateral (select p.sampleid from sample_cat_ref_dists p
+localhost postgres@postgres( #         where
+localhost postgres@postgres( #         p.sampleid != m.sampleid
+localhost postgres@postgres( #         and abs(m.refdist1 - p.refdist1) < (1609.34 * 1.0)
+localhost postgres@postgres( #         and abs(m.refdist2 - p.refdist2) < (1609.34 * 1.0)
+localhost postgres@postgres( #         and abs(m.refdist3 - p.refdist3) < (1609.34 * 1.0)
+localhost postgres@postgres( #         and st_distance(p.st_geompoint, m.st_geompoint) <= (1609.34 * 1.0)
+localhost postgres@postgres( #         limit 1
+localhost postgres@postgres( #     ) t2 on true;
+ mcount | tcount 
+--------+--------
+ 150000 | 145105
+(1 row)
+
+Time: 140871.058 ms (02:20.871)
+
+localhost:5432 postgres@postgres=# 
+localhost:5432 postgres@postgres=# 
+localhost:5432 postgres@postgres=# select count(m.sampleid) as mcount,
+localhost postgres@postgres- #         count(t2.sampleid) as tcount
+localhost postgres@postgres- #     from sample_cat_ref_dists m
+localhost postgres@postgres- #     left join lateral (select p.sampleid from sample_cat_ref_dists p
+localhost postgres@postgres( #         where
+localhost postgres@postgres( #         p.sampleid != m.sampleid
+localhost postgres@postgres( #         and st_distance(p.st_geompoint, m.st_geompoint) <= (1609.34 * 1.0)
+localhost postgres@postgres( #         limit 1
+localhost postgres@postgres( #     ) t2 on true;
+ mcount | tcount 
+--------+--------
+ 150000 | 145105
+(1 row)
+
+Time: 3770321.721 ms (01:02:50.322)
+localhost:5432 postgres@postgres=# 
+
+
+localhost:5432 postgres@postgres=# select count(m.sampleid) as mcount,
+localhost postgres@postgres- #         count(t2.sampleid) as tcount
+localhost postgres@postgres- #     from sample_cat_ref_dists m
+localhost postgres@postgres- #     left join lateral (select p.sampleid from sample_cat_ref_dists p
+localhost postgres@postgres( #         where
+localhost postgres@postgres( #         p.sampleid != m.sampleid
+localhost postgres@postgres( #         and abs(m.refdist1 - p.refdist1) < (1609.34 * 0.1)
+localhost postgres@postgres( #         and abs(m.refdist2 - p.refdist2) < (1609.34 * 0.1)
+localhost postgres@postgres( #         and abs(m.refdist3 - p.refdist3) < (1609.34 * 0.1)
+localhost postgres@postgres( #         and st_distance(p.st_geompoint, m.st_geompoint) <= (1609.34 * 0.1)
+localhost postgres@postgres( #         limit 1
+localhost postgres@postgres( #     ) t2 on true;
+
+ mcount | tcount 
+--------+--------
+ 150000 | 117033
+(1 row)
+
+Time: 760640.202 ms (12:40.640)
+
+ocalhost:5432 postgres@postgres=# 
+localhost:5432 postgres@postgres=# 
+localhost:5432 postgres@postgres=# select count(m.sampleid) as mcount,
+localhost postgres@postgres- #         count(t2.sampleid) as tcount
+localhost postgres@postgres- #     from sample_cat_ref_dists m
+localhost postgres@postgres- #     left join lateral (select p.sampleid from sample_cat_ref_dists p
+localhost postgres@postgres( #         where
+localhost postgres@postgres( #         p.sampleid != m.sampleid
+localhost postgres@postgres( #         and st_distance(p.st_geompoint, m.st_geompoint) <= (1609.34 * 0.1)
+localhost postgres@postgres( #         limit 1
+localhost postgres@postgres( #     ) t2 on true;
+ mcount | tcount 
+--------+--------
+ 150000 | 117033
+(1 row)
+
+Time: 19664676.473 ms (05:27:44.676)
--- postgresql/manual_cattrilatapprox_query.sql
+++ postgresql/manual_cattrilatapprox_query.sql
@@ -66,3 +66,19 @@ SELECT EXTRACT(EPOCH FROM timestamptz '2013-07-01 12:00:00') -
 
 SELECT EXTRACT(EPOCH FROM timestamptz '2013-07-20 12:00:07' -
        	 	          timestamptz '2013-07-20 12:00:00');
+
+
+
+select count(m.sampleid) as mcount,
+        count(t2.sampleid) as tcount
+    from sample_cat_ref_dists m
+    left join lateral (select p.sampleid from sample_cat_ref_dists p
+        where
+        p.category = 14
+        and abs(m.refdist1 - p.refdist1) < (1609.34 * 0.1)
+        and abs(m.refdist2 - p.refdist2) < (1609.34 * 0.1)
+        and abs(m.refdist3 - p.refdist3) < (1609.34 * 0.1)
+        and st_distance(p.st_geompoint, m.st_geompoint) <= (1609.34 * 0.1)
+        limit 1
+    ) t2 on true
+    where m.category = 15;
