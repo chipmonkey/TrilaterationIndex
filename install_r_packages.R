a936f4882e0963676709deea78931d1d3f164c4b
Chip Lynch <clynch@manifold.ai>
2021-07-06 12:33:33 -0700

    Add package installer for Thesis dependencies

---
+++ install_r_packages.R
@@ -0,0 +1,12 @@
+install.packages('knitr')
+install.packages(c('base64enc', 'digest', 'htmltools', 'jsonlite', 'rmarkdown', 'tinytex'))
+install.packages('plotrix')
+
+# MathTheory.rmd
+install.packages(c('ggplot2', 'ggforce', 'rdist'))
+
+# Experiment.rmd
+install.packages(c('mapproj', 'maps'))
+
+# SQL_Results.rmd
+install.packages(c('data.table', 'tidyverse', 'gridExtra'))
